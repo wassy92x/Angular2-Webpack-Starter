9e7ae29f3b9929d9501d50909377b8daf8b80b63
wassy92x <github@fabian.alias.wassy.de>
2016-12-23 17:57:54 +0100

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from "@angular/core";
+import "../styles.global";
+
+@Component({
+    selector: "app-main",
+    templateUrl: "./app.component.html",
+    styleUrls: ["app.component.scss"]
+})
+export class AppComponent {
+
+}
