23d6904c6caf6faa15789ec347c0e58fe0e8a586
wassy92x <github@fabian.alias.wassy.de>
2016-12-25 12:59:56 +0100

    added router + example component

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -23,7 +23,8 @@ module.exports = {
         loaders: [
             {
                 test: /\.ts$/,
-                loaders: ["awesome-typescript-loader", "angular2-template-loader"]
+                loaders: ["awesome-typescript-loader", "angular2-template-loader?keepUrl=true"],
+                exclude: [/\.(spec|e2e)\.ts$/]
             },
             {
                 test: /\.html$/,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {AppComponent} from "./app.component";
+import * as routes from "./app.routing";
+import {RouterModule} from "@angular/router";
+import {HomeComponent} from "./home/home.component";
+import {LocationStrategy, HashLocationStrategy} from "@angular/common";
+
 @NgModule({
     imports: [
-        BrowserModule
+        BrowserModule,
+        RouterModule.forRoot(routes)
     ],
     declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent
     ],
-    bootstrap: [AppComponent]
+    providers: [
+        {provide: LocationStrategy, useClass: HashLocationStrategy}
+    ],
+    bootstrap: [
+        AppComponent
+    ]
 })
 export class AppModule {
 }
