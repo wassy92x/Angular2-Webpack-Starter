01da8c9a2a3aa919f1c337bbae6d4163bab232ef
wassy92x <github@fabian.alias.wassy.de>
2016-12-25 22:58:34 +0100

    moved routing to own module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {AppComponent} from "./app.component";
-import * as routes from "./app.routing";
-import {RouterModule} from "@angular/router";
+import {AppRoutingModule} from "./app-routing.module";
 import {HomeComponent} from "./home/home.component";
 import {LocationStrategy, HashLocationStrategy} from "@angular/common";
 
 @NgModule({
     imports: [
         BrowserModule,
-        RouterModule.forRoot(routes)
+        AppRoutingModule
     ],
     declarations: [
         AppComponent,
