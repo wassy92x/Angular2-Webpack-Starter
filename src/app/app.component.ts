828e3bfb14c20ce59ac4eea7384c26267ce8c50e
wassy92x <info@wassy-sws.de>
2017-03-06 21:31:03 +0100

    fixed version incompatibility

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import "../styles.global";
 
 @Component({
     selector: "app-main",
-    templateUrl: "./app.component.html",
+    templateUrl: "app.component.html",
     styleUrls: ["app.component.scss"]
 })
 export class AppComponent {
